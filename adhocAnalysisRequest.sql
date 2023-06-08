ffe65d2fbc9348e06dd93d56e804f0bb4900ff3f
Naveen <gsnaveen@yahoo.com>
2023-06-08 12:22:58 -0700

    Update adhocAnalysisRequest.sql

--- adhocAnalysisRequest.sql
+++ adhocAnalysisRequest.sql
@@ -1,18 +1,20 @@
 Freeze/copy Base data
 split processing into steps
-layer the steps
+layer the steps for consistency 
 
 Step 1 bring in the base data (multiple sources)
 Step 2 align grain of the data
-Step 3 Join multiple entities
+Step 3 Join related entities
 Step 4 Create one big flat table
 Step 5 Create summary entities
-Step 6 Analyze the Summary
+Step 6 Analyze the numeric Summary
 Step 7 find corelations
+Step 8 Attribute summary, distint #Of Values, #null rows, #Of Value rows
 
 
 cautions:
   cartesian joins
   one to many relationships
   Nulls
-  conversions
+  conversions/ casting
+  
