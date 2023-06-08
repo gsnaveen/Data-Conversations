7ba9577972fb025b8c62910ad544f24cb8298050
Naveen <gsnaveen@yahoo.com>
2023-06-08 12:15:42 -0700

    Create adhocAnalysisRequest.sql

---
+++ adhocAnalysisRequest.sql
@@ -0,0 +1,11 @@
+Freeze/copy Base data
+split processing into steps
+layer the steps
+
+Step 1 bring in the base data (multiple sources)
+Step 2 align grain of the data
+Step 3 Join multiple entities
+Step 4 Create one big flat table
+Step 5 Create summary entities
+Step 6 Analyze the Summary
+Step 7 find corelations
